8c0a1fa14e2aed2c20c502b112f544a5c3f34d09
ramzymulla <rza@uoregon.edu>
2025-02-18 09:07:39 -0800

    init

---
+++ oop.py
@@ -0,0 +1,34 @@
+class Pet:
+
+    def __init__(self, the_name, the_color, the_sex):
+        ## Data ##
+        self.name = the_name
+        self.color = the_color
+        self.sex = the_sex
+        self._owner = None
+
+    def vocalize(self):
+        pass
+
+    def get_adopted(self,thing):
+        self.vocalize()
+        self._owner = thing
+
+    def i_love(self, thing):
+        print(f'I love {thing}!')
+
+class Cat(Pet):
+
+    ## Methods ##
+    def vocalize(self):
+        print("Meeeooowwwww")
+
+
+class Dog(Pet):
+
+    def vocalize(self):
+        print("Woof")
+
+    
+
+
